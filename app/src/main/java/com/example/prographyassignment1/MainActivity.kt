48cad8381dcb5ea2e93aabf2e9dae20bfa4e8d01
chooeeh <chchgml10@gmail.com>
2020-04-08 17:22:00 +0900

    2020/04/08 bottom nav와 4개의 Fragment 연결

--- app/src/main/java/com/example/prographyassignment1/MainActivity.kt
+++ app/src/main/java/com/example/prographyassignment1/MainActivity.kt
@@ -2,11 +2,49 @@ package com.example.prographyassignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        replaceFragment(HomeFragment())
+
+        bottom_nav.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.item_home -> {
+                    replaceFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.item_promotion -> {
+                    replaceFragment(PromotionFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.item_trip -> {
+                    replaceFragment(TripFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.item_mypage -> {
+                    replaceFragment(MypageFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
