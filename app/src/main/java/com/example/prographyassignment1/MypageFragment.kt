80339912b1ea220f14f7ce0f40faeb1b76860c00
chooeeh <chchgml10@gmail.com>
2020-04-10 18:17:02 +0900

    2020/04/10 My 계정 clone 완료

--- app/src/main/java/com/example/prographyassignment1/MypageFragment.kt
+++ app/src/main/java/com/example/prographyassignment1/MypageFragment.kt
@@ -1,12 +1,15 @@
 package com.example.prographyassignment1
 
-import android.content.Context
-import android.net.Uri
+
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
+import com.google.android.material.appbar.AppBarLayout
+import kotlinx.android.synthetic.main.fragment_mypage.*
 
 
 class MypageFragment : Fragment() {
@@ -17,6 +20,11 @@ class MypageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mypage, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
     }
 
 }
